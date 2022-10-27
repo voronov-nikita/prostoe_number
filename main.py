d2568049fce04adfbaca253f2cddb4f171cffb82
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2022-10-27 15:34:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+# Простое число
 def prost(x):
-    for i in range(2, x//2+1):
+    for i in range(2, x // 2 + 1):
         return False
     return True
+
 
 x = int(input("Введите любое число: \n"))
 print(prost(x))
 
+
+# минимальное число
+def minimum(*x):  # * говорит о перечислениях из списка(который был создан из последовательности x
+    m = x[0]
+    for i in x:
+        if i < m:  # Для максимума меняем на >
+            m = i
+    return m
+
+
+a, b, c, d, e = 2, 5, 7, 22, 42
+print(minimum(a, b, c, d, e))   #2
